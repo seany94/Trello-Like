55043e80cd62585c7681877cb50876847acb4ef9
sean chan <sean@seans-MacBook-Air.local>
2019-03-16 01:14:18 +0800

    Insert Ajax response object details into cards, and working on matching col id

--- build/prod/src/my-app.js
+++ build/prod/src/my-app.js
@@ -3,26 +3,57 @@ import "../node_modules/@polymer/iron-flex-layout/iron-flex-layout-classes.js";
 import './card.js';
 
 class MyApp extends PolymerElement {
-  constructor() {
-    super();
+  static get properties() {
+    return {
+      data: {
+        type: Object,
+        notify: true
+      }
+    };
+  }
+
+  ready() {
+    super.ready();
+
+    this._computeData();
+  }
+
+  _computeData() {
+    this._getResource({
+      url: './materials/db.json',
+
+      onLoad(e) {
+        this.set('data', JSON.parse(e.target.responseText));
+      }
+
+    });
+  }
+
+  _getResource(rq) {
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', rq.onLoad.bind(this));
+    xhr.open('GET', rq.url);
+    xhr.send();
   }
 
   static get template() {
     return html`
-      <style is="custom-style" include="iron-flex iron-flex-alignment"></style>
-      <style>
-        .col { width: 500px; }
-      </style>
-      <div class="layout horizontal center-center">
-        <div class="col">
-          Column 1
-          <card-element></card-element>
-        </div>
-        <div class="col">
-          Column 2
-        </div>
-      </div>
-    `;
+
+            <style is="custom-style" include="iron-flex iron-flex-alignment"></style>
+                <style>
+                .col { width: 500px; }
+                .col-header { display: block; background-color: teal; border-style: inset; padding: 10px; text-align: center; font-size: 20px; color: white;}
+            </style>
+
+                <div class="layout horizontal">
+                    <template is="dom-repeat" items="{{data.columns}}">
+                        <div class="col">
+                            <div class="col-header">{{item.title}}</div>
+                            <card-element></card-element>
+                        </div>
+                    </template>
+                </div>
+        `;
   }
 
 }
--- src/card.js
+++ src/card.js
@@ -3,17 +3,66 @@ import '@polymer/paper-card/paper-card.js';
 import '@polymer/paper-button/paper-button.js';
 
 export class Card extends PolymerElement {
+
+    constructor(){
+        super();
+    }
+
+    static get properties() {
+      return {
+        data: {
+          type: Object,
+          notify: true
+        }
+      };
+    }
+
+    ready() {
+      super.ready();
+      this._computeData();
+    }
+
+
+    _computeData() {
+     this._getResource({
+        url: './materials/db.json',
+        onLoad(e) {
+            this.set('data', JSON.parse(e.target.responseText));
+        }
+      });
+    }
+
+    _getResource(rq) {
+      let xhr = new XMLHttpRequest();
+      xhr.addEventListener('load', rq.onLoad.bind(this));
+      xhr.open('GET', rq.url);
+      xhr.send();
+    }
+
+    isSameId(data) {
+        console.log(data)
+        return data.id == data.columnId;
+    }
+
   static get template() {
     return html`
-    <paper-card heading="Emmental" image="http://placehold.it/350x150/FFC107/000000" alt="Emmental">
-      <div class="card-content">
-        Emmentaler or Emmental is a yellow, medium-hard cheese that originated in the area around Emmental, Switzerland. It is one of the cheeses of Switzerland, and is sometimes known as Swiss cheese.
-      </div>
-      <div class="card-actions">
-        <paper-button>Share</paper-button>
-        <paper-button>Explore!</paper-button>
-      </div>
-    </paper-card>
+
+    <style>
+        .card { width: 500px; border-bottom: 5px solid teal;}
+    </style>
+
+    <template is="dom-repeat" items="{{data.cards}}" filter="isSameId" observe="data.id">
+        <paper-card class="card" heading="{{item.title}}">
+          <div class="card-content">
+            {{item.description}}
+            Column ID: {{item.columnId}}
+          </div>
+          <div class="card-actions">
+            <paper-button>Edit</paper-button>
+            <paper-button>Delete</paper-button>
+          </div>
+        </paper-card>
+    </template>
     `;
   }
 }
